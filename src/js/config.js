79ab7d8a018669dde544919472fb579a67b967ea
Zomer Gregorio <zomerzxc.19@gmail.com>
2021-05-15 03:17:10 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd394b7a4-5ad5-449e-9777-69f40321f042';
+export const MODAL_CLOSE_SEC = 1.5;
